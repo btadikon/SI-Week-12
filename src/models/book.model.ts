481e0058fefd1c8359fc4b9105cdb2b55a27bbb6
btadikon <btadikon@uncc.edu>
2025-04-13 23:14:57 -0400

    first commit

---
+++ src/models/book.model.ts
@@ -0,0 +1,70 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Book extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  author: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  isbn: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  category: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  publishDate: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  stock: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  description: string;
+
+
+  constructor(data?: Partial<Book>) {
+    super(data);
+  }
+}
+
+export interface BookRelations {
+  // describe navigational properties here
+}
+
+export type BookWithRelations = Book & BookRelations;
